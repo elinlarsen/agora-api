b808bf19958220e31999b474e41562aaecf0e035
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-12 19:00:02 +0200

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose     = require('mongoose');
 
 mongoose
-  .connect('process.env.MONGODB_URI', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
