8fac45101d6d19fa1f024ae25ab33d894d97756b
yacinebe <yacineberrada@gmail.com>
2019-07-12 18:16:57 +0200

    first router

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const logger       = require('morgan');
-const path         = require('path');
-const cors         = require('cors')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const cors = require("cors");
 
-
-
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(cors({
- credentials: true,
- origin: [process.env.SITE_URL]
-}));
- 
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.SITE_URL]
+  })
+);
 
 // default value for title local
-app.locals.title = 'Agora';
+app.locals.title = "Agora";
 
-const index = require('./routes/index');
-app.use('/', index);
-
+const index = require("./routes/index");
+app.use("/", index);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const models = ["users", "agoras", "projects", "messages"];
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.send({data: "helloworld"})
+const getCollectionEndpoints = models.map(model => "/" + model);
+const getOneByIdEndpoints = models.map(model => "/" + model + "/:id");
+const createEndpoints = models.map(model => "/" + model + "/new");
+const updateEndpoints = models.map(model => "/" + model + "/update");
+const deleteEndpoints = models.map(model => "/" + model + "/delete");
+
+/* GET routes */
+
+router.get("/", (req, res, next) => {
+  res.send({ data: "helloworld" });
+});
+
+router.get(getCollectionEndpoints, (req, res, next) => {
+  console.log(req.query.q);
+
+  res.send({
+    data: "You have reached " + req.path.substring(1),
+    data2: "Would you like a collection of " + req.path.substring(1) + "?"
+  });
+});
+
+router.get(getOneByIdEndpoints, (req, res, next) => {
+  res.send({
+    data: "You have reached " + req.path.substring(1),
+    data2: "Would you like item " + req.params.id + "?"
+  });
+});
+
+/* POST routes */
+
+router.post(createEndpoints, (req, res, next) => {
+  res.send({
+    data: "You have reached " + req.path.substring(1),
+    data2: "Would you like to create an item?"
+  });
+});
+
+/* PATCH routes */
+
+router.patch(updateEndpoints, (req, res, next) => {
+  res.send({
+    data: "You have reached " + req.path.substring(1),
+    data2: "Would you like to update an item?"
+  });
+});
+
+/* DELETE routes */
+
+router.delete(deleteEndpoints, (req, res, next) => {
+  res.send({
+    data: "You have reached " + req.path.substring(1),
+    data2: "Would you like to delete an item?"
+  });
 });
 
 module.exports = router;
