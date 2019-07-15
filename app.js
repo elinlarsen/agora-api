db2410656280b1713bed3e29d628e53ad9580d63
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-15 17:13:14 +0200

    adding cors

--- app.js
+++ app.js
@@ -21,12 +21,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.SITE_URL]
-  })
-);
+
+/*app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'], // <== this will be the URL of our React app (it will be running on port 3000)
+//[process.env.SITE_APP]
+}));*/
+
+var allowedOrigins = ['http://localhost:3000',
+                      'http://agoraapp.com'];
+app.use(cors({
+  credentials: false,
+  origin: function(origin, callback){// allow requests with no origin  (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 // default value for title local
 app.locals.title = "Agora";
