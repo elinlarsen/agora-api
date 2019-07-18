752602b6487d42e1b5b745357220117450030924
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-18 13:15:25 +0200

    changing cors policies

--- app.js
+++ app.js
@@ -29,18 +29,20 @@ app.use(cookieParser());
 }));*/
 
 var allowedOrigins = [process.env.SITE_APP];
-app.use(cors({
-  credentials: false,
-  origin: function(origin, callback){// allow requests with no origin  (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
+// app.use(cors({
+//   credentials: false,
+//   /*origin: function(origin, callback){// allow requests with no origin  (like mobile apps or curl requests)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//     */
+//   }));
+//}));
 
 
 // default value for title local
