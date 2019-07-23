e39fc31e2deef6da3c8260d528b0474508145a4b
elinlarsen <elin_larsen1@hotmail.fr>
2019-07-23 11:12:49 +0200

    adding deployed URL front app to cors in back

--- app.js
+++ app.js
@@ -29,27 +29,10 @@ app.use(cookieParser());
 /*var allowedOrigins = [process.env.SITE_APP];*/
 var corsOptions ={
   credentials: true,
-  origin:  [process.env.SITE_APP], 
+  origin:  [process.env.SITE_APP, process.env.SITE_APP_DEPLOYED], 
 }
 app.use(cors(corsOptions));
 
-
-// app.use(cors());
-
-
-// app.use(cors({
-//   credentials: false,
-//   /*origin: function(origin, callback){// allow requests with no origin  (like mobile apps or curl requests)
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1){
-//       var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//     */
-//   }));
-//}));
 
 //SESSION
 app.use(session({
